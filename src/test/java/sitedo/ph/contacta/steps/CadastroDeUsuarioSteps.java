3cca9069c492c82155b77b26dec6db94e3eaeaf1
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2014-06-30 22:19:44 -0300

    ADD: classe com os steps

--- src/test/java/sitedo/ph/contacta/steps/CadastroDeUsuarioSteps.java
+++ src/test/java/sitedo/ph/contacta/steps/CadastroDeUsuarioSteps.java
@@ -10,5 +10,23 @@ import sitedo.ph.contacta.services.UsuarioService;
 
 public class CadastroDeUsuarioSteps {
 
+	private String	mensagemExibida;
 
+	@Given("o eleitor José da Silva deseja acompanhar seu representante no governo")
+	public void givenOEleitorJoseDaSilvaDesejaAcompanharSeuRepresentanteNoGoverno() {
+
+	}
+
+	@When("ele preencher o formulário de cadastro com o nome de usuário $usuario e a senha $senha e submetê-lo")
+	public void whenElePreencherOFormularioDeCadastroComONomeDeUsuariojoseEASenhadasilvaESubmetelo(String usuario, String senha) {
+
+		UsuarioService usuarioService = new UsuarioService();
+		mensagemExibida = usuarioService.cadastrar(usuario, senha);
+	}
+
+	@Then("o sistema deve exibir a seguinte mensagem: $mensagem")
+	public void thenOSistemaDeveExibirASeguinteMensagemOUsuariojoseComASenhadasilvaFoiCadastradoComSucesso(String mensagem) {
+
+		assertThat(mensagemExibida).isEqualTo(mensagem);
+	}
 }
